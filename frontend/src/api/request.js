125e7c8919967c17dd822788a5c2bb7e2a0b276b
Bainianzzz <3036349123@qq.com>
2024-09-15 11:57:40 +0800

    Reset the timeout for Axios

--- frontend/src/api/request.js
+++ frontend/src/api/request.js
@@ -1,8 +1,6 @@
-import axios from "axios";
-const api = axios.create(
-	{ 
-		baseURL: "/api", //这里配置的是后端服务提供的接口
-		timeout: 15000 
-	}
-);
-export default api;
+import axios from 'axios'
+const api = axios.create({
+  baseURL: '/api', //这里配置的是后端服务提供的接口
+  timeout: 60 * 1000
+})
+export default api
